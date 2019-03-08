89595edd5025257c42456babcbdb01b9bed4414e
qianwx1993 <zhouxiao0629@qq.com>
2019-03-08 15:47:57 +0800

    测试spring实战@PropertiesSource代码，未完成

---
+++ src/main/java/com/qwx/myProject/config/ExpressiveConfig.java
@@ -0,0 +1,17 @@
+package com.qwx.myProject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @Version 1.0
+ * @Since JDK1.7
+ * @Author Qian
+ * @Company Bangsun
+ * @Date 2019/3/8 15:42
+ */
+@Configuration
+@PropertySource("classpath:/other/app.propertis")
+public class ExpressiveConfig {
+
+}
