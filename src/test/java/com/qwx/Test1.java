4809ead5a318f68b69d7115a7d6ccaaf8ec3ba8e
qianwenxue <qianwx@bsfit.com.cn>
2020-01-02 15:55:20 +0800

    测试,jvm字节码

---
+++ src/test/java/com/qwx/Test1.java
@@ -0,0 +1,17 @@
+package com.qwx;
+
+/**
+ * @Version 1.0
+ * @Since JDK1.8
+ * @Author Qian
+ * @Company Bangsun
+ * @Date 2019/12/31 17:23
+ */
+public class Test1 {
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 5;
+		int c = b-a;
+		System.out.println(c);
+	}
+}
---
+++ src/test/java/com/qwx/Test2.java
@@ -0,0 +1,28 @@
+package com.qwx;
+
+/**
+ * 查看i++与++i的不同
+ * @Version 1.0
+ * @Since JDK1.8
+ * @Author Qian
+ * @Company Bangsun
+ * @Date 2019/12/31 17:23
+ */
+public class Test2 {
+	public static void main(String[] args) {
+		new Test2().method1();
+		new Test2().method2();
+	}
+
+	public void method1(){
+		int i = 1;
+		int a = i++;
+		System.out.println(a); //打印1
+	}
+
+	public void method2(){
+		int i = 1;
+		int a = ++i;
+		System.out.println(a);//打印2
+	}
+}
